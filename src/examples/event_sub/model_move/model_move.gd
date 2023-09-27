c74a8eeb46a5f61c8ace8f3960d045f6bab0a987
Hue <hue.dynamics@gmail.com>
2023-09-27 03:01:46 -0400

    Update model_move.gd

--- src/examples/event_sub/model_move/model_move.gd
+++ src/examples/event_sub/model_move/model_move.gd
@@ -4,9 +4,6 @@ var authentication_token: String = "";
 
 func _ready():
 	$GDVtubeStudio.client_response.connect(_on_gd_vtube_studio_server_response);
-	#This is an example of how one might load an existing authentication token
-	#for VTubeStudio, from a prior session
-	#This saves you the trouble of needing to ask for a new token each time
 	if (FileAccess.file_exists("user://GDVTS_demo.dat")):
 		var file = FileAccess.open("user://GDVTS_demo.dat", FileAccess.READ)
 		var content = file.get_as_text()
@@ -20,9 +17,6 @@ func _on_gd_vtube_studio_server_response(message):
 	print(message);
 	if (message is Dictionary):
 		match (message.messageType):
-#			ResponseTypes.APIError:
-#				if (message.data.authenticated == false):
-#					$GDVtubeStudio.request_authentication_token()
 			ResponseTypes.AuthenticationTokenResponse:
 				if (message.data.authenticationToken):
 					authentication_token = message.data.authenticationToken;
@@ -30,12 +24,13 @@ func _on_gd_vtube_studio_server_response(message):
 					var file = FileAccess.open("user://GDVTS_demo.dat", FileAccess.WRITE)
 					file.store_string(authentication_token);
 					file.close()
+					$GDVtubeStudio.request_authentication();
 			ResponseTypes.AuthenticationResponse:
 				if (message.data.authenticated == false):
 					$GDVtubeStudio.request_authentication_token()
 					return;
 				#When ready, subscribe
-				_on_request_event_subscription_pressed();
+				_request_event_subscription();
 			ResponseTypes.ModelMovedEvent:
 				var client_size = get_viewport().size;
 				var upper_bound_width = client_size.x;
@@ -45,7 +40,7 @@ func _on_gd_vtube_studio_server_response(message):
 				$Recticle.position = Vector2(converted_x, converted_y);
 	return;
 
-func _on_request_event_subscription_pressed():
+func _request_event_subscription():
 	var request = RequestTypes.EventSubscriptionRequestArguments;
 	request.eventName = "ModelMovedEvent"
 	request.subscribe = true
